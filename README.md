20a43958d2d34ba6eb4f67880b5685588ba84462
Pablo Klijnjan <pablo@factordata.com>
2015-09-13 17:29:07 +0300

    Added push job to queue

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # data-collector
 
 sudo docker build -t data-collector .
-sudo docker run -d -it --rm -v /host/path/to/opt/:/opt data-collector
+sudo docker run --net=host -d -it -v /host/path/to/opt/:/opt data-collector
+
+`net=host` option in run, will give access to localhost, where rabbitmq is expected to be running. If we link at some pint
+to the docker container of rabbitmq we can disregard this and connect directly to that
+
 
 folder struct of mounted volume should be:
 
@@ -16,5 +20,5 @@ To check it works, place a file under opt/collector/files and it should be copie
 
 You can inspect logs:
 
-sudo docker exec -i -t 3d00312236bb bas
+sudo docker exec -i -t <id> bash
 less /var/log/collector.log
--- collect.py
+++ collect.py
@@ -2,9 +2,12 @@ import os
 import shutil
 import logging
 
+import pika
 import pyinotify
 
 # pylint: disable=invalid-name,no-self-use
+RABBITMQ_ADDR = 'localhost'
+QUEUE_NAME = 'processor'
 
 
 class EventHandler(pyinotify.ProcessEvent):
@@ -14,12 +17,25 @@ class EventHandler(pyinotify.ProcessEvent):
         shutil.copy(event.pathname, destination)
         msg = 'Copied file {} to {}'.format(filename, destination)
         logging.info(msg)
+        channel.basic_publish(exchange='',
+                              routing_key=QUEUE_NAME,
+                              body=destination)
+        logging.info('Sent file {} to process'.format(destination))
 
 
 if __name__ == '__main__':
     logging.basicConfig(filename='/var/log/collector.log', level=logging.DEBUG)
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(RABBITMQ_ADDR)
+    )
+    logging.info('Connected to {}'.format(RABBITMQ_ADDR))
+    channel = connection.channel()
+    # Make sure queue is there
+    channel.queue_declare(queue=QUEUE_NAME)
     wm = pyinotify.WatchManager()
     mask = pyinotify.IN_CREATE  # watched events
     notifier = pyinotify.Notifier(wm, EventHandler())
     wm.add_watch('/opt/collector/files', mask, rec=True)
     notifier.loop()
+    channel.close()
+    connection.close()
