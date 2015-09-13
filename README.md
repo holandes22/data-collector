e2b631f46f88c5535fa1d7f6d0e03da1a7ad479d
Pablo Klijnjan <pablo@factordata.com>
2015-09-13 10:06:58 +0300

    Listening on folder changes to collect files

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # data-collector
 
 sudo docker build -t data-collector .
-sudo docker run -it --rm -v /host/docker-volumes/opt/:/opt data-collector
+sudo docker run -d -it --rm -v /host/path/to/opt/:/opt data-collector
+
+folder struct of mounted volume should be:
+
+opt/
+    /collector
+        /files
+    /processor
+        /queue
+        /processed
+
+To check it works, place a file under opt/collector/files and it should be copied to opt/processor/queue
+
+You can inspect logs:
+
+sudo docker exec -i -t 3d00312236bb bas
+less /var/log/collector.log
--- collect.py
+++ collect.py
@@ -1,11 +1,25 @@
+import os
 import shutil
+import logging
+
+import pyinotify
+
+# pylint: disable=invalid-name,no-self-use
 
 
-def collect(source, destination):
-    shutil.copy(source, destination)
+class EventHandler(pyinotify.ProcessEvent):
+    def process_IN_CREATE(self, event):
+        filename = os.path.basename(event.pathname)
+        destination = os.path.join('/opt/processor/queue/', filename)
+        shutil.copy(event.pathname, destination)
+        msg = 'Copied file {} to {}'.format(filename, destination)
+        logging.info(msg)
 
 
 if __name__ == '__main__':
-    SRC = '/opt/collector/files.tar.gz'
-    DST = '/opt/processor/files.tar.gz'
-    collect(SRC, DST)
+    logging.basicConfig(filename='/var/log/collector.log', level=logging.DEBUG)
+    wm = pyinotify.WatchManager()
+    mask = pyinotify.IN_CREATE  # watched events
+    notifier = pyinotify.Notifier(wm, EventHandler())
+    wm.add_watch('/opt/collector/files', mask, rec=True)
+    notifier.loop()
