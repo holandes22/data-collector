0db83bf606d1f6528fc212f07dd9e4b3bcd6d92d
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-11-22 17:55:26 +0200

    Removed rethinkdb stuff

--- collect.py
+++ collect.py
@@ -8,7 +8,6 @@ from urllib.parse import urlparse
 
 import pika
 import requests
-import rethinkdb
 from bs4 import BeautifulSoup
 from etcdc.client import Client
 
@@ -17,7 +16,6 @@ from etcdc.client import Client
 
 ETCD_SERVICE_HOST = os.getenv('ETCD0_SERVICE_HOST', 'localhost')
 RABBITMQ_SERVICE_HOST = os.getenv('RABBITMQ_SERVICE_HOST', 'localhost')
-RETHINKDB_SERVICE_HOST = os.getenv('RETHINKDB_SERVICE_HOST', 'localhost')
 QUEUE_NAME = 'processor'
 PROCESSOR_FILES_PATH = '/opt/data/processor/queue'
 
@@ -54,7 +52,7 @@ class AuditsDownloader(object):
                     resp.raise_for_status()
                 except requests.exceptions.HTTPError as err:
                     msg = 'Skipping file {} due to error: {} ({})'.format(
-                        filename, err.message, resp.status_code)
+                        filename, err, resp.status_code)
                     logging.warn(msg)
                     continue
                 with open(dst_filepath, 'wb') as fdescriptor:
@@ -70,7 +68,6 @@ class Collector(object):
         self.downloader = AuditsDownloader(dst_path)
         self.is_downloading = False
         self.etcd_client = Client(address=ETCD_SERVICE_HOST, port='2379')
-        self.rconn = rethinkdb.connect(RETHINKDB_SERVICE_HOST, '28015')
         with self.blocking_channel() as channel:
             # Make sure queue is there
             channel.queue_declare(queue=QUEUE_NAME)
@@ -93,7 +90,6 @@ class Collector(object):
             channel.basic_publish(exchange='',
                                   routing_key=QUEUE_NAME,
                                   body=filepath)
-        rethinkdb.db('data').table('collected').insert({'filepath': filepath}).run(self.rconn)
         logging.info('Placed in queue file {}'.format(filepath))
 
     def set_url(self):
@@ -135,9 +131,7 @@ if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logging.debug('etcd host: {}'.format(ETCD_SERVICE_HOST))
     logging.debug('rabbitmq host: {}'.format(RABBITMQ_SERVICE_HOST))
-    logging.debug('rethinkdb host: {}'.format(RETHINKDB_SERVICE_HOST))
 
     # Start loop runner
     collector = Collector(PROCESSOR_FILES_PATH)
     collector.run_periodically()
-
